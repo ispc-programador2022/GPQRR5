9800d801892af32206c159e99fba5bbe7df37153
agustinpic72 <agustinpiccoli@gmail.com>
2022-11-01 15:28:10 -0300

    Buscador basico por nombre con algunas plantillas

--- scraper/scraper/urls.py
+++ scraper/scraper/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from gestionScrapers.views import search, searchProducts
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('busqueda_productos/', searchProducts),
+    path('buscar/',search),
 ]
