083aa63e775ab2a4d304415efc9c5c71ecdb84ff
agustinpic72 <agustinpiccoli@gmail.com>
2022-11-11 12:31:31 -0300

    script para transferir los datos de la base de datos a excel

---
+++ scraper/bbdd_to_excel.py
@@ -0,0 +1,18 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scraper.settings')
+import django
+django.setup()
+from gestionScrapers.models import Product
+import pandas as pd
+
+productos = Product.objects.all()
+df = {
+    'Página':[producto.page for producto in productos],
+    'Titulo':[producto.title for producto in productos],
+    'Precio':[producto.price for producto in productos],
+    'Marcas':[producto.brand for producto in productos],
+    'Links':[producto.link for producto in productos]
+    }
+df = pd.DataFrame(df)
+
+df.to_excel('data.xlsx')
